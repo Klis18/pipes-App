a354ff8c14f7fffd838d50c78eeddc1ef2f55ca0
Klis18 <klisnp18@gmail.com>
2024-04-09 18:25:43 -0500

    Pipes no comuner parte 1

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+
+  // i18n Select 
+  public name: string = 'Fernando';
+  public gender: 'male'| 'female' = 'male';
+  public invitacionMap = {
+    'male' : 'invitarlo',
+    'female' : 'invitarla'
+  }
+
+  changeClient(): void{
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
+
+  //i18n Plural
+  public clients: string[]=['Maria','José','Alejandra','Belén','Mario','Luisa','Anthony'];
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos 2 clientes esperando',
+    'other': 'tenemos # clientes esperando'
+  }
+  deleteClient(): void{
+    this.clients.shift();
+  }
 }
