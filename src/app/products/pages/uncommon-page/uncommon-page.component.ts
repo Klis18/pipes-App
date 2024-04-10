076712e47b4bcd116bfc5d67419b3b0addb070be
Klis18 <klisnp18@gmail.com>
2024-04-10 18:07:39 -0500

    Desarrollo de otros pipes no coomunes

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -32,4 +33,24 @@ export class UncommonPageComponent {
   deleteClient(): void{
     this.clients.shift();
   }
+
+  //keyvalue pipe
+
+  public person = {
+    name: "Fernando",
+    age:36,
+    address: 'Ottawa, Canada'
+  }
+
+  //Async Pipe
+  public myObservableTimer = interval(2000).pipe(
+    tap(value => console.log('tap:', value))
+  );
+
+  public promiseValue: Promise <string> = new Promise( (resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data en la promesa.')
+    }, 3500);
+  })
+
 }
